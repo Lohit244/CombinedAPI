80f818e7a33a4a6ed778685155755cc54ff6d9cb
Pawan Agarwal <agarwalpawan6089@gmail.com>
2022-04-02 21:13:10 +0530

    switches to mongodb atlas

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ dotenv.config({ path: "./env" });
 const app = require("./app");
 const mongoose = require("mongoose");
 console.log(process.env.JWT_SECRET);
-mongoose.connect(`mongodb://localhost:27017/NapsUserDB`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@napsuserdb.n9gvp.mongodb.net/NapsUserDB?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to DB");
 });
 
-app.listen(3000, () => {
-  console.log("Server listening at port 3000");
+app.listen(4000, () => {
+  console.log("Server listening at port 4000");
 });
