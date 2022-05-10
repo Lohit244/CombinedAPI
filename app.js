333777773f5a3327fc2b4d503408fbfa731c52b0
Lohit244 <lohit244@gmail.com>
2022-05-10 15:52:58 +0530

    origin * for backend

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const cors = require("cors");
 const app = express();
 app.use(helmet());
 app.use(xss());
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}))
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/v1/users", userRouter);
